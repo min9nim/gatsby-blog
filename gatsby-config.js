390a26c2dd4139648f511c3bdc7b867f8ca1340d
mgsong <mgsong@jjsoft.kr>
2020-05-09 20:11:02 +0900

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `LearnApplyShare`,
     author: {
       name: `Keating`,
-      summary: `who lives and works in Korea building useful things.`,
+      summary: `who loves to write code and build useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
