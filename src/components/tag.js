07482dfd175c8a4e9d26b758c2d22d0e0298f9aa
mgsong <mgsong@jjsoft.kr>
2020-05-12 20:33:37 +0900

    add margin prop

--- src/components/tag.js
+++ src/components/tag.js
@@ -2,12 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import "./tag.scss"
 
-export default function Tag({ tag, count }) {
+export default function Tag({ tag, count, margin = "7px" }) {
   return (
     <Link
       to={"/tags/archives?tag=" + tag}
       className="tag"
-      style={{ fontSize: count + 14 + "px" }}
+      style={{ fontSize: count + 14 + "px", margin }}
     >
       {tag}
     </Link>
