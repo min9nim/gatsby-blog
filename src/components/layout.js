9aab4da0cd8ac8403e53fb37326bdc37f79377e1
mgsong <mgsong@jjsoft.kr>
2020-05-12 10:55:17 +0900

    theme 변경시 깜빡임 문제 수정

--- src/components/layout.js
+++ src/components/layout.js
@@ -51,65 +51,70 @@ const Layout = ({ location, title, children }) => {
   )
 
   function changeTheme(theme) {
-    window.setTheme(theme)
     setTheme(theme)
+    window.setTheme(theme)
   }
 
   return (
     <div
       style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         color: "var(--textNormal)",
         background: "var(--bg)",
         transition: "color 0.2s ease-out, background 0.2s ease-out",
         minHeight: "100vh",
       }}
     >
-      <header
+      <div
         style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: "2.625rem",
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
-        {location.pathname === rootPath ? indexHeader : postHeader}
-        <Toggle
-          icons={{
-            checked: (
-              <img
-                src={moon}
-                alt="moon"
-                width="16"
-                height="16"
-                role="presentation"
-                style={{ pointerEvents: "none" }}
-              />
-            ),
-            unchecked: (
-              <img
-                src={sun}
-                alt="sun"
-                width="16"
-                height="16"
-                role="presentation"
-                style={{ pointerEvents: "none" }}
-              />
-            ),
+        <header
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: "2.625rem",
           }}
-          checked={theme === "dark"}
-          onChange={e => changeTheme(e.target.checked ? "dark" : "light")}
-        />
-      </header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
+        >
+          {location.pathname === rootPath ? indexHeader : postHeader}
+          <Toggle
+            icons={{
+              checked: (
+                <img
+                  src={moon}
+                  alt="moon"
+                  width="16"
+                  height="16"
+                  role="presentation"
+                  style={{ pointerEvents: "none" }}
+                />
+              ),
+              unchecked: (
+                <img
+                  src={sun}
+                  alt="sun"
+                  width="16"
+                  height="16"
+                  role="presentation"
+                  style={{ pointerEvents: "none" }}
+                />
+              ),
+            }}
+            checked={theme === "dark"}
+            onChange={e => changeTheme(e.target.checked ? "dark" : "light")}
+          />
+        </header>
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
     </div>
   )
 }
