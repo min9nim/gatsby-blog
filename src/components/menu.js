69a6dfac545057750ba5a64c2ab6c05bc382e61a
mgsong <mgsong@jjsoft.kr>
2020-05-12 14:23:17 +0900

    update menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,14 +1,23 @@
 import React from "react"
+import { Link } from "gatsby"
 import "./menu.scss"
 
 export default () => {
   return (
     <nav className="menu">
       <ul>
-        <li>About</li>
-        <li>Category</li>
-        <li>Tags</li>
-        <li>Archives</li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/categories">Category</Link>
+        </li>
+        <li>
+          <Link to="/tags">Tags</Link>
+        </li>
+        <li>
+          <Link to="/archives">Archives</Link>
+        </li>
       </ul>
     </nav>
   )
