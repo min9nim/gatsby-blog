168ab80ddc33d1d893087afe8dc5034aee38436f
mgsong <mgsong@jjsoft.kr>
2020-05-12 20:16:12 +0900

    use tag component

--- src/components/tag-cloud.js
+++ src/components/tag-cloud.js
@@ -1,19 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
+import Tag from "./tag"
 import "./tag-cloud.scss"
 
 export default ({ tags }) => {
   return (
     <section className="tag-cloud">
       {Object.entries(tags).map(([key, value]) => (
-        <Link
-          to={"/tags/archives?tag=" + key}
-          key={key}
-          className="tag"
-          style={{ fontSize: value + 14 + "px" }}
-        >
-          {key}
-        </Link>
+        <Tag tag={key} count={value} key={key} />
       ))}
     </section>
   )
