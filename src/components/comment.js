d500c489b0f0b6ac700fcc0bf97810f68249343c
mgsong <mgsong@jjsoft.kr>
2020-05-14 11:29:03 +0900

    add log

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,10 +1,13 @@
 // https://velog.io/@iamchanii/build-a-blog-with-gatsby-and-typescript-part-4
 import React, { createRef, useEffect } from "react"
+import logger from "../../build/logger"
 
 export default function Comment({ repo, theme = "github-light" }) {
   const containerRef = createRef()
+  logger.debug("Comment render")
 
   useEffect(() => {
+    logger.debug("Comment effect", repo)
     const utterances = document.createElement("script")
     const attributes = {
       src: "https://utteranc.es/client.js",
@@ -19,7 +22,7 @@ export default function Comment({ repo, theme = "github-light" }) {
       utterances.setAttribute(key, value)
     })
     containerRef.current.appendChild(utterances)
-  }, [repo])
+  }, [])
 
   return <div ref={containerRef} />
 }
