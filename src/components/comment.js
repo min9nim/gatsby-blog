01e19806bfcc25df386cbbcdf2009bc2a7ddf1be
mgsong <mgsong@jjsoft.kr>
2020-05-14 12:01:12 +0900

    sync dark mode with utterences

--- src/components/comment.js
+++ src/components/comment.js
@@ -22,8 +22,14 @@ export default function Comment({ repo, theme = "github-light" }) {
     Object.entries(attributes).forEach(([key, value]) => {
       utterances.setAttribute(key, value)
     })
-    containerRef.current.appendChild(utterances)
-  }, [])
 
-  return <div ref={containerRef} />
+    if (containerRef.current.hasChildNodes()) {
+      // sync dark mode with utterences
+      containerRef.current.innerHTML = ""
+    }
+
+    containerRef.current.appendChild(utterances)
+  }, [theme])
+
+  return <div id="comment" ref={containerRef} />
 }
