90b3f82d89be4dcc13c9d953273f3f65bac2a732
mgsong <mgsong@jjsoft.kr>
2020-05-15 12:04:42 +0900

    fix lint warning

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react"
-import { Link, graphql } from "gatsby"
+import React from "react"
+import { graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
