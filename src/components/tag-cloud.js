c93b90d52ff2cf96b578596d4e6a5ee3f8657c1b
min9nim <min9nim@gmail.com>
2020-08-19 08:40:24 +0900

    태그 가나다 정렬

--- src/components/tag-cloud.js
+++ src/components/tag-cloud.js
@@ -1,13 +1,22 @@
-import React from "react"
+import React, { useContext } from "react"
 import Tag from "./tag"
+import { go } from "mingutils"
+import { map, sort } from "ramda"
+import LoggerContext from '../context/logger'
 import "./tag-cloud.scss"
 
 export default ({ tags }) => {
+  const logger = useContext(LoggerContext).addTags('tag-cloud')
   return (
     <section className="tag-cloud">
-      {Object.entries(tags).map(([key, value]) => (
-        <Tag tag={key} count={value} key={key} />
-      ))}
+      {go(
+        Object.entries(tags),
+        sort((a, b) => {
+          // logger.debug(a[0], b[0])
+          return a[0] < b[0] ? -1 : 1
+        }),
+        map(([key, value]) => <Tag tag={key} count={value} key={key} />)
+      )}
     </section>
   )
 }
