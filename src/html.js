f2a499cf861eea780602979dd2fd3eee1fbaa2c1
keating <min9nim@gmail.com>
2022-06-01 23:21:02 +0900

    그냥 head 태그 안에 구글광고 삽입

--- src/html.js
+++ src/html.js
@@ -12,6 +12,11 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3221090343920987"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body {...props.bodyAttributes}>
         <script
